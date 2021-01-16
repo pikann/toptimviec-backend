3ba7f70713b7e22692c3f030c5407d80e5905030
pikann <hvhai22@gmail.com>
2021-01-16 17:53:02 +0700

    Deploy to heroku

--- run.py
+++ run.py
@@ -5,8 +5,7 @@ CONFIG_PATH = os.path.join(ROOT_DIR, 'configuration.conf')
 from app import *
 
 app = Flask(__name__)
+app.register_blueprint(bp)
 
 if __name__ == "__main__":
-
-    app.register_blueprint(bp)
     app.run(debug=True)

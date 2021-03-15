24c970353dcd0c39a9b496cdc4875d5b97bc48a5
pikann <hvhai22@gmail.com>
2021-03-15 20:50:14 +0700

    Add feature mail

--- run.py
+++ run.py
@@ -3,11 +3,9 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 CONFIG_PATH = os.path.join(ROOT_DIR, 'configuration.conf')
 
 from controller import *
-from flask_socketio import SocketIO
 
 app = Flask(__name__)
 app.register_blueprint(bp)
-socketio = SocketIO(app)
 
 if __name__ == "__main__":
-    socketio.run(app=app, debug=True)
+    app.run(debug=True)

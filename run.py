e65ba53f821c07b73e8d78584c0daa35e19bb130
pikann <hvhai22@gmail.com>
2021-03-16 23:57:57 +0700

    Test feature socket

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-CONFIG_PATH = os.path.join(ROOT_DIR, 'configuration.conf')
+CONFIG_PATH = os.path.join(ROOT_DIR, 'configuration.config')
 
 from controller import *
 

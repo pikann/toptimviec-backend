e0cbae2f74955252054c525d9b27a7e522f8b148
pikann <hvhai22@gmail.com>
2021-03-23 15:51:59 +0700

    Reorganize file

--- services/__init__.py
+++ services/__init__.py
@@ -4,4 +4,4 @@ import yagmail
 client = MongoClient('mongodb+srv://pikann22:Wt7GsHbs7LmFWVT7@toptimviec.4zz7i.mongodb.net/test')
 db = client.toptimviec
 yag = yagmail.SMTP(user='toptimviec@gmail.com', password='TopTimViec1')
-email_form = open("email_form.html", "r").read().replace("\n", " ")
+email_form = open("templates/email_form.html", "r").read().replace("\n", " ")

7cfc9306b9c040e8b6ca30a6d3f6e73d7f3952a5
pikann <hvhai22@gmail.com>
2021-05-08 18:08:24 +0700

    chore: add avatar default

--- models/Applicant.py
+++ models/Applicant.py
@@ -8,7 +8,7 @@ class Applicant:
         self._id = ObjectId()
         self.name = ""
         self.gender = True
-        self.avatar = ""
+        self.avatar = "https://res.cloudinary.com/pikann22/image/upload/v1615044354/toptimviec/TCM27Jw1N8ESc1V0Z3gfriuG1NjS_hXXIww7st_jZ0bFz3xGRjKht7JXzfLoU_ZelO4KPiYFPi-ZBVZcR8wdQXYHnwL5SDFYu1Yf7UBT4jhd9d8gj0lCFBA5VbeVp9SveFfJVKRcLON-OyFX_kxrs3f.png"
         self.dob = datetime.datetime.now()
         self.place = ""
         self.hashtag = {h: 0 for h in list_hashtag}
--- models/Employer.py
+++ models/Employer.py
@@ -7,7 +7,7 @@ class Employer:
         self._id = ObjectId()
         self.name = ""
         self.bio = ""
-        self.avatar = ""
+        self.avatar = "https://res.cloudinary.com/pikann22/image/upload/v1615044354/toptimviec/TCM27Jw1N8ESc1V0Z3gfriuG1NjS_hXXIww7st_jZ0bFz3xGRjKht7JXzfLoU_ZelO4KPiYFPi-ZBVZcR8wdQXYHnwL5SDFYu1Yf7UBT4jhd9d8gj0lCFBA5VbeVp9SveFfJVKRcLON-OyFX_kxrs3f.png"
         self.hashtag = {h: 0 for h in list_hashtag}
 
     def id(self):

1373df437e791a7ad6d5106cb503dbca930757ed
pikann <hvhai22@gmail.com>
2021-05-16 17:53:04 +0700

    fix: fix url encode error

--- models/RefreshToken.py
+++ models/RefreshToken.py
@@ -1,6 +1,5 @@
-import base64
-import os
-
+import random
+import string
 from bson.objectid import ObjectId
 import datetime
 
@@ -8,7 +7,7 @@ import datetime
 class RefreshToken:
     def __init__(self, id_user, role):
         self._id = ObjectId()
-        self.key = base64.b64encode(os.urandom(24)).decode('utf-8')
+        self.key = ''.join(random.choice(string.ascii_uppercase + string.digits) for i in range(60))
         self.token_expiration = datetime.datetime.utcnow() + datetime.timedelta(days=7)
         self.id_user = id_user
         self.role = role

d847cad427fbfb63c96c11381dcee4539816ea8c
pikann <hvhai22@gmail.com>
2021-05-20 22:39:38 +0700

    feat: improve socket

--- routes/socket.py
+++ routes/socket.py
@@ -1,13 +1,19 @@
+from bson import ObjectId
 from flask_socketio import SocketIO, join_room
 from flask import request
-from util.token import check_token
+
+from services.refresh_token import get_refresh_token
 
 socketio = SocketIO(async_mode='eventlet', cors_allowed_origins="*", pingInterval=30000, pingTimeout=30000)
 
 
 @socketio.on('connect')
 def connect():
-    token=check_token(request.headers.get('Authorization').split(' ')[1])
-    if token is None:
+    key = request.headers.get('Authorization').split(' ')[1].split('.')
+    try:
+        token = get_refresh_token(ObjectId(key[0]), key[1])
+        if token is None:
+            return
+        join_room(str(token["id_user"]))
+    except:
         return
-    join_room(str(token.id_user))

b704859c549713b6be3f51a7cd46c42738a64d54
pikann <hvhai22@gmail.com>
2021-05-25 22:55:19 +0700

    feat: implement get employer for admin

--- routes/admin.py
+++ routes/admin.py
@@ -1,12 +1,14 @@
-from flask import abort
+from flask import request, abort
 from routes import bp
 from services.auth import token_auth
 from services.admin import count_user, count_cv, count_post, count_post_unexpired, count_employer, count_applicant
+from services.employer import list_employer_admin
 
 
 @bp.route('/general-info', methods=['GET'])
 @token_auth.login_required(role="admin")
 def get_general_infomation():
+    global rs
     try:
         rs = {
             "count_user": count_user(),
@@ -20,3 +22,21 @@ def get_general_infomation():
         abort(403)
 
     return rs
+
+
+@bp.route('/admin/employer', methods=['GET'])
+@token_auth.login_required(role="admin")
+def get_list_employer_admin():
+    global page, name
+    try:
+        name = str(request.args.get('name', default=""))
+        page = int(request.args.get('page', default=0))
+    except:
+        abort(400)
+
+    try:
+        return {"list_employer": list_employer_admin(name, page)}
+    except:
+        abort(403)
+
+
